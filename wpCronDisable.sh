d9f7024e8424296fbab5b7385b585be46b756fa2
Igor Andrade <igorhenriquehdbr@gmail.com>
2022-02-26 15:20:20 -0300

    Create wpCronDisable.sh

---
+++ wpCronDisable.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Author: Igor Andrade
+# 26/02/2022
+
+DATA=$(date +%d-%m-%Y)
+LOGS="/var/log/script-wpcron-disable.log"
+> $LOGS
+for filez in $(find /home/ -iname "wp-config.php") 
+do 
+echo -e "checking file $filez"
+	if egrep DISABLE_WP_CRON $filez >> /dev/null 2>&1
+	then
+		echo -e "Alredy disabled on $filez on $DATA" | tee -a $LOGS
+	else
+		echo -e "Enabling on $filez on $DATA" | tee -a $LOGS
+		sed -i "/DB_NAME/a define\(\'DISABLE_WP_CRON\'\, true\)\;" $filez
+	fi
+done
